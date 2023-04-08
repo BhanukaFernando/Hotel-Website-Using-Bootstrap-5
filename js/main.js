cfe4d33bd8ad2a91962d8193a1c0842044aba309
bhanukadil <110652615+bhanukadil@users.noreply.github.com>
2023-04-09 01:54:38 +0530

    banner section  complet
    add swiper
    a form  not complet(responsive viwe not complet)

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@
 // nav hide
 
 // swipper slider
+var swiper = new Swiper('.myswiper', {
+    direction: 'vertical',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+    },
+    autoplay:{
+        delay:3500,
+    },
+  });
 
 // counter design
 
