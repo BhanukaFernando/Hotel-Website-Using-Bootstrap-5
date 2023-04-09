887fcd5e07912b6fb18bc47c1f4372dcec89e4c9
bhanukadil <110652615+bhanukadil@users.noreply.github.com>
2023-04-10 02:07:50 +0530

    complet Booking area and banner section  some  points and responsive css /css js scrollell

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
 // Header scroll
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
 
-// nav hide
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+})
 
 // swipper slider
 var swiper = new Swiper('.myswiper', {
     direction: 'vertical',
     loop: true,
     pagination: {
-      el: '.swiper-pagination',
-      clickable:true,
+        el: '.swiper-pagination',
+        clickable: true,
     },
-    autoplay:{
-        delay:3500,
+    autoplay: {
+        delay: 3500,
     },
-  });
+});
 
 // counter design
 
